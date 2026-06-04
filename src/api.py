703cd032f678876f680abb1687754b1e37969af7
dosed <e.nmitenin@gmail.com>
2026-06-04 22:41:30 +0300

    add a helper function

--- src/api.py
+++ src/api.py
@@ -5,8 +5,26 @@ load_dotenv('.env')
 
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 
-url = 'https://newsapi.org/v2/everything'
+def _build_query(
+    include_keywords: list[str],
+    exclude_keywords: list[str],
+) -> str:
+    query = include_keywords.copy()
 
-params = {
-    'apiKey': ''
-}
+    for word in exclude_keywords:
+        query.append(f'-{word}')
+
+    return ' '.join(query)
+
+def fetch_articles(
+
+) -> list[dict]:
+    url = 'https://newsapi.org/v2/everything'
+
+    params = {
+        'apiKey': NEWS_API_KEY,
+        'q':include_keywords
+    }
+
+    return None
+
