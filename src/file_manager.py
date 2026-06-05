39c21544ab07fabf918876128bfa604e9a41656f
dosed <e.nmitenin@gmail.com>
2026-06-05 22:16:08 +0300

    add logger for file_manager

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,8 +1,11 @@
 import json
 from pathlib import Path
 from datetime import datetime
+from src.config import logger
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 def save_articles(
         articles: list[dict],
@@ -10,8 +13,16 @@ def save_articles(
 ) -> None:
     file_path = BASE_DIR / 'data' / filename
 
+    logger.info(
+        f'Saving articles to {file_path}'
+    )
+
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(articles, f, ensure_ascii=False, indent=4)
+
+    logger.info(
+        f'Successfully saved {len(articles)} articles'
+    )
 
 def generate_filename(date:str) -> str:
     return (
