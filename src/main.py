b5c5eb54c240b377cde196cfe3219a592488b089
dosed <e.nmitenin@gmail.com>
2026-06-05 21:48:53 +0300

    update draft run

--- src/main.py
+++ src/main.py
@@ -2,18 +2,22 @@
 # from src.file_manager import save_articles
 # from src.utils import get_today_date
 # from src.utils import normalize_articles
+# from src.file_manager import generate_filename
 #
+# date = '2026-06-05'
 #
 # articles = fetch_articles(
-#     '2026-06-04',
+#     date,
 #     ['python'],
 #     []
 # )
 #
 # articles = normalize_articles(articles)
 #
+# filename = generate_filename(date)
+#
 #
 # save_articles(
 #     articles,
-#     'news.json'
+#     filename
 # )
