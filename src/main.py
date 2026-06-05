6baeb1a09ba4cf75d61b2e321f5dd8850870cd46
dosed <e.nmitenin@gmail.com>
2026-06-05 21:33:47 +0300

    draft run

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # from src.api import fetch_articles
 # from src.file_manager import save_articles
 # from src.utils import get_today_date
+# from src.utils import normalize_articles
 #
 #
 # articles = fetch_articles(
@@ -8,6 +9,9 @@
 #     ['python'],
 #     []
 # )
+#
+# articles = normalize_articles(articles)
+#
 #
 # save_articles(
 #     articles,
